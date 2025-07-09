42215949fc1b6bb7a06a9b0feef5348603793ee3
Praveen230389 <Praveenkumar230389@gmail.com>
2025-07-10 02:09:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,11 @@ terraform {
   }
 }
 
+resource "aws_key_pair" "deployer" {
+  key_name   = "my-key"
+  public_key = file("~/.ssh/my-key.pub")
+}
+
 resource "aws_instance" "example" {
   ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (example)
   instance_type = "t2.micro"
